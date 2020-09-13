759cccfd737e568501ffce0977f5cdf3f11907e2
MalwarePerseus <66173883+MalwarePerseus@users.noreply.github.com>
2020-09-13 22:23:21 +0530

    Add files via upload

---
+++ precache-manifest.c6159446e36fbf601caf4851dad6a3ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbaf1993b08f719e04ce17f6ae78dfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad48b234121917e6866c",
+    "url": "/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "9be690f550a0b35cf8a3",
+    "url": "/static/css/main.96b2f562.chunk.css"
+  },
+  {
+    "revision": "ad48b234121917e6866c",
+    "url": "/static/js/2.4118c14c.chunk.js"
+  },
+  {
+    "revision": "4f89695379d3cca452a09d6ce7984607",
+    "url": "/static/js/2.4118c14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be690f550a0b35cf8a3",
+    "url": "/static/js/main.36c41643.chunk.js"
+  },
+  {
+    "revision": "ce7b350515357ce83319",
+    "url": "/static/js/runtime-main.b56792ae.js"
+  }
+]);
